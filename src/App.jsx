5398c775caac85179d821378be84e44d79df6cdf
jcabiativaibero <jcabiati@estudiante.ibero.edu.co>
2026-05-03 19:00:48 -0500

    change app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/services";
-import Login from "./pages/login"; 
+import Login from "./pages/login";
+import Publish from "./pages/Publish";
 
 function App() {
   return (
     <Routes>
-      {/* 🔥 HOME REAL */}
       <Route path="/" element={<Home />} />
-
-      {/* OTRAS PÁGINAS */}
       <Route path="/services" element={<Services />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/publish" element={<Publish />} />  
     </Routes>
   );
 }
